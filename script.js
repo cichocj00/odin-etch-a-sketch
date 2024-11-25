a64a9f324b572e895f1a581d73f6fa4cc8fcac9e
cichocj0 <cichocjagh@gmail.com>
2024-11-25 18:24:03 +0100

    Create functions for painting squares

--- script.js
+++ script.js
@@ -11,5 +11,16 @@ for (let i = 0; i < squaresQuantity; ++i) {
     const square = document.createElement('div');
     square.classList.add('square');
     square.style.maxWidth = `${(containerSize - 2) / gridSize}px`
+    square.addEventListener('mouseover', (e) => paintRandomColor(e.target))
+
     container.appendChild(square);
+}
+
+function paintBlack(square) {
+    square.style.backgroundColor = 'black';
+}
+
+function paintRandomColor(square) {
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    square.style.backgroundColor = `#${randomColor}`;
 }
